ae61f07f749b8d048e0536abce28e5bdfe31cf80
Tumas Bajoras <hanniph@gmail.com>
2011-10-25 18:49:07 +0300

    adding stub scrobbling + play time tracking

--- extension/content/js/app/onScrobble.coffee
+++ extension/content/js/app/onScrobble.coffee
@@ -1,20 +1,30 @@
 onScrobble =
-  pre: (code, object) ->
-    "var onScrobble = #{code}();\n onScrobble.load();"
+  pre: (code) ->
+    "var onScrobble = #{code}();"
 
-  inject: (code)->
+  inject: (code, generic)->
     # no main, call directly
-    wrap = "function main(){\n#{this.pre(code)}\n}"
+    wrap = "function main(){\n#{this.pre(code)}\n onScrobble.scrobbler = #{generic}();\nonScrobble.load();\n}"
     scriptNode = document.createElement("script")
     scriptNode.textContent = "#{wrap};\nmain();"
     document.head.appendChild(scriptNode)
+
+  scrobbler: ->
+    # duration must be in seconds
+    canSubmit: (timePlayed, duration) ->
+      timePlayed >= 30 and (timePlayed >= duration/2 or timePlayed >= 60*4)
+
+    secondify: (ts) -> Math.round Number(ts/1000)
+
+    submit: (track) ->
+      # todo
 
   soundcloud: ->
     events:
       play: "onAudioPlay"
       pause: "onAudioPause"
-
-    timestamp: -> Math.round Number(new Date()) / 1000
+      finish: "onAudioFinish"
+      playStart: "onAudioPlayStart"
 
     artist: (username, track) ->
       trackInfo = track
@@ -23,25 +33,57 @@ onScrobble =
         trackInfo = "#{username} - #{track}"
       trackInfo
 
-    #canSubmit: (timePlayed, duration) ->
-    #  timePlayed >= duration / 2 or timePlayed >= 4 * 60 
-
     load: ->
+      # State 
       idle_time  = 0
-      start_play = undefined
       idle_start = undefined
-      current_track = undefined
+      start_time = undefined
+      track = undefined
+      duration = 0
+      finish_submit = no
 
       $(document).bind onScrobble.events.play, (info) ->
-        trackName = onScrobble.artist(info.track.user.username, info.track.title)
-        console.log("audio played: #{trackName}")
+        # stop idle time counting if was idle
+        if idle_start
+          idle_time += info.timeStamp - idle_start
+          idle_start = undefined
 
       $(document).bind onScrobble.events.pause, (info) ->
+        # set idle time timestamp
+        idle_start = info.timeStamp
+
+      $(document).bind onScrobble.events.finish, (info) ->
+        play_time = onScrobble.scrobbler.secondify(info.timeStamp - idle_time - start_time)
+
+        if onScrobble.scrobbler.canSubmit(play_time, duration)
+          console.log "Submitting: #{track}"
+          finish_submit = yes
+        
+      $(document).bind onScrobble.events.playStart, (info) ->
+        # track changes here
         trackName = onScrobble.artist(info.track.user.username, info.track.title)
-        console.log("audio paused: #{trackName}")
+
+        idle_time += info.timeStamp - idle_start if idle_start
+        play_time = onScrobble.scrobbler.secondify(info.timeStamp - idle_time - start_time)
+
+        # submit(track) if not finish_submit and canSubmit() 
+        if not finish_submit and onScrobble.scrobbler.canSubmit(play_time, duration)
+          console.log "Submitting: #{track}"
+
+        # submit now playing for new track
+        console.log "Now playing: #{trackName}"
+        
+        # reset
+        idle_time = 0
+        idle_start = undefined
+        start_time = info.timeStamp
+        finish_submit = false
+        track = trackName
+        duration = onScrobble.scrobbler.secondify(info.track.duration)
 
 (exports ? this).onScrobble = onScrobble
 
 jQuery ($) ->
   injected = onScrobble.soundcloud.toString()
-  onScrobble.inject injected
+  generic = onScrobble.scrobbler.toString()
+  onScrobble.inject injected, generic
--- extension/content/js/app/onScrobble.js
+++ extension/content/js/app/onScrobble.js
@@ -1,24 +1,34 @@
 (function() {
   var onScrobble;
   onScrobble = {
-    pre: function(code, object) {
-      return "var onScrobble = " + code + "();\n onScrobble.load();";
+    pre: function(code) {
+      return "var onScrobble = " + code + "();";
     },
-    inject: function(code) {
+    inject: function(code, generic) {
       var scriptNode, wrap;
-      wrap = "function main(){\n" + (this.pre(code)) + "\n}";
+      wrap = "function main(){\n" + (this.pre(code)) + "\n onScrobble.scrobbler = " + generic + "();\nonScrobble.load();\n}";
       scriptNode = document.createElement("script");
       scriptNode.textContent = "" + wrap + ";\nmain();";
       return document.head.appendChild(scriptNode);
+    },
+    scrobbler: function() {
+      return {
+        canSubmit: function(timePlayed, duration) {
+          return timePlayed >= 30 && (timePlayed >= duration / 2 || timePlayed >= 60 * 4);
+        },
+        secondify: function(ts) {
+          return Math.round(Number(ts / 1000));
+        },
+        submit: function(track) {}
+      };
     },
     soundcloud: function() {
       return {
         events: {
           play: "onAudioPlay",
-          pause: "onAudioPause"
-        },
-        timestamp: function() {
-          return Math.round(Number(new Date()) / 1000);
+          pause: "onAudioPause",
+          finish: "onAudioFinish",
+          playStart: "onAudioPlayStart"
         },
         artist: function(username, track) {
           var trackInfo;
@@ -29,20 +39,47 @@
           return trackInfo;
         },
         load: function() {
-          var current_track, idle_start, idle_time, start_play;
+          var duration, finish_submit, idle_start, idle_time, start_time, track;
           idle_time = 0;
-          start_play = void 0;
           idle_start = void 0;
-          current_track = void 0;
+          start_time = void 0;
+          track = void 0;
+          duration = 0;
+          finish_submit = false;
           $(document).bind(onScrobble.events.play, function(info) {
-            var trackName;
-            trackName = onScrobble.artist(info.track.user.username, info.track.title);
-            return console.log("audio played: " + trackName);
+            if (idle_start) {
+              idle_time += info.timeStamp - idle_start;
+              return idle_start = void 0;
+            }
           });
-          return $(document).bind(onScrobble.events.pause, function(info) {
-            var trackName;
+          $(document).bind(onScrobble.events.pause, function(info) {
+            return idle_start = info.timeStamp;
+          });
+          $(document).bind(onScrobble.events.finish, function(info) {
+            var play_time;
+            play_time = onScrobble.scrobbler.secondify(info.timeStamp - idle_time - start_time);
+            if (onScrobble.scrobbler.canSubmit(play_time, duration)) {
+              console.log("Submitting: " + track);
+              return finish_submit = true;
+            }
+          });
+          return $(document).bind(onScrobble.events.playStart, function(info) {
+            var play_time, trackName;
             trackName = onScrobble.artist(info.track.user.username, info.track.title);
-            return console.log("audio paused: " + trackName);
+            if (idle_start) {
+              idle_time += info.timeStamp - idle_start;
+            }
+            play_time = onScrobble.scrobbler.secondify(info.timeStamp - idle_time - start_time);
+            if (!finish_submit && onScrobble.scrobbler.canSubmit(play_time, duration)) {
+              console.log("Submitting: " + track);
+            }
+            console.log("Now playing: " + trackName);
+            idle_time = 0;
+            idle_start = void 0;
+            start_time = info.timeStamp;
+            finish_submit = false;
+            track = trackName;
+            return duration = onScrobble.scrobbler.secondify(info.track.duration);
           });
         }
       };
@@ -50,8 +87,9 @@
   };
   (typeof exports !== "undefined" && exports !== null ? exports : this).onScrobble = onScrobble;
   jQuery(function($) {
-    var injected;
+    var generic, injected;
     injected = onScrobble.soundcloud.toString();
-    return onScrobble.inject(injected);
+    generic = onScrobble.scrobbler.toString();
+    return onScrobble.inject(injected, generic);
   });
 }).call(this);
